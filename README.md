0ffa576fcf889db48cd734dd3382a81129599ff7
Christoffer Pettersson <corgrath@gmail.com>
2019-08-19 19:05:29 +0200

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# Player.GG Simple Game Overlay
+
+The Player.GG Simple Game Overlay is a very simple, but flexible, tool to show various site content over your game.
+
+[Click here for the official site for more information.](https://www.player.gg/tools/simple-game-overlay/)
+
+## Releases
+
+[Releases to download can be found here.](https://github.com/playergg/simple-game-overlay/releases/)
+
+## Changes
+
+### Version 1.0.0
+
+ - Initial version
+
+## Additional Copyright information
+
+ - Certain Icons are made by [Dave Gandy](https://www.flaticon.com/authors/dave-gandy) and are downloaded from [www.flaticon.com](https://www.flaticon.com/), licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/).
---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+ * Player.GG Simple Game Overlay
+ */
+
+const path = require("path");
+const {app, BrowserWindow, ipcMain} = require("electron");
+
+function onReady() {
+
+    const win = new BrowserWindow({
+        title: "Player.GG Simple Game Overlay",
+        icon: path.join(__dirname, "assets/icon.ico"),
+        width: 600,
+        height: 400,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
+        resizable: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // Open the developer tools
+//     win.webContents.openDevTools();
+
+    // and load the index.html of the app.
+    win.loadFile("docs/index.html");
+//     win.loadURL("https://playergg.github.io/simple-game-overlay/");
+
+//     win.on("page-title-updated", (evt) => {
+//         evt.preventDefault();
+//     });
+
+    ipcMain.on("pgg-minimize-app", () => {
+        win.minimize();
+    });
+
+    ipcMain.on("pgg-close-app", () => {
+        win.close();
+    });
+
+}
+
+app.on("ready", onReady);
